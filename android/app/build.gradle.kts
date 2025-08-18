1d6ba956cb52a3ea2b91732d3a16a595e17c99dc
sebahattinn <sebogaming123@gmail.com>
2025-08-18 10:20:13 +0300

    başlangıç commit'i

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,34 @@
+android {
+    namespace = "com.sebahattin.socialmedia"
+
+    defaultConfig {
+        applicationId = "com.sebahattin.socialmedia"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+// Repository ve build directory ayarların aynen kalabilir
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
